4c943a7f0ecdc6d3f094ef79f31a2686ff0f69ee
anderson1030 <chananderson1@gmail.com>
2024-06-21 02:24:46 +0800

    inital commit

---
+++ backend/src/settings/useAppSettings.js
@@ -0,0 +1,8 @@
+const useAppSettings = () => {
+  let settings = {};
+  settings['idurar_app_email'] = 'cs@marketingpro.com.hk';
+  settings['idurar_base_url'] = 'https://marketingpro.com.hk';
+  return settings;
+};
+
+module.exports = useAppSettings;
---
+++ frontend/src/modules/AuthModule/SideContent.jsx
@@ -0,0 +1,107 @@
+import { Space, Layout, Divider, Typography } from 'antd';
+import logo from '@/style/images/company-logo.png';
+import useLanguage from '@/locale/useLanguage';
+import { useSelector } from 'react-redux';
+import { selectLangDirection } from '@/redux/translate/selectors';
+
+const { Content } = Layout;
+const { Title, Text } = Typography;
+
+export default function SideContent() {
+  const translate = useLanguage();
+  const langDirection = useSelector(selectLangDirection);
+
+  return (
+    <Content
+      style={{
+        padding: '150px 30px 30px',
+        width: '100%',
+        maxWidth: '400px',
+        margin: '0 auto',
+      }}
+      className="sideContent"
+    >
+      <div style={{ width: '100%' }}>
+        <img
+          src={logo}
+          alt="IDURAR ERP CRM"
+          style={{ margin: '0 auto 40px', display: 'block' }}
+          height={200}
+          width={200}
+        />
+        <div className="space40"></div>
+
+        <div className="space20"></div>
+
+        <Divider />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          {/* <img
+            src={logo1}
+            alt="Logo1"
+            style={{
+              margin: '0 15px',
+              display: 'block',
+              float: 'left',
+              width: '48px',
+              filter: 'grayscale(1)',
+              mixBlendMode: 'multiply',
+              opacity: '0.8',
+            }}
+            height={48}
+            width={48}
+          />
+          <img
+            src={logo2}
+            alt="Logo2"
+            style={{
+              margin: '0 15px',
+              display: 'block',
+              float: 'left',
+              width: '48px',
+              filter: 'grayscale(1)',
+              mixBlendMode: 'multiply',
+              opacity: '0.8',
+            }}
+            height={48}
+            width={48}
+          />
+          <img
+            src={logo3}
+            alt="Logo3"
+            style={{
+              margin: '0 15px',
+              display: 'block',
+              float: 'left',
+              width: '48px',
+              filter: 'grayscale(1)',
+              mixBlendMode: 'multiply',
+              opacity: '0.8',
+            }}
+            height={48}
+            width={48}
+          />
+          <img
+            src={logo4}
+            alt="Logo4"
+            style={{
+              margin: '0 15px',
+              display: 'block',
+              float: 'left',
+              width: '48px',
+              filter: 'grayscale(1)',
+              mixBlendMode: 'multiply',
+              opacity: '0.8',
+            }}
+            height={48}
+            width={48}
+          /> */}
+        </div>
+      </div>
+    </Content>
+  );
+}
